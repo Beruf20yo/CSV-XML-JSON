90fb8350efb95148a97a3e71bbe59c493272982d
RinatG <rinat_galimov_1992@mail.ru>
2023-09-01 23:07:55 +0300

    changes

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,10 +3,9 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
 
-//        CSV_JSON_Parser parser = new CSV_JSON_Parser("src/main/resources/data.csv");
-//        parser.work();
-        XML_JSON_Parser parser = new XML_JSON_Parser("src/main/resources/data.xml");
+        CSV_JSON_Parser parser = new CSV_JSON_Parser("src/main/resources/data.csv");
         parser.work();
+
 
     }
 }
